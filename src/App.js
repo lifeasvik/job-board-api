051c3452cdee4a265f6b6c2bd6cc5c342a929c93
Vik Birdi <vikbirdi@Viks-MacBook-Pro.local>
2020-07-17 13:02:30 -0500

    initialized project, added temp state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'fontsource-roboto';
+
+import Jobs from './components/Jobs';
+
+const mockJobs = [
+  {title: 'Software Engineer', company: 'Google'},
+  {title: 'Junior Developer', company: 'Microsoft'},
+  {title: 'Software Developer', company: 'Facebook'},
+  {title: 'Senior Software Engineer', company: 'Google'},
+  {title: 'Entry Level Full Stack Developer', company: 'Indeed'},
+  {title: 'Software Engineer Level ', company: 'Specs Wine Delivery'},
+  {title: 'Junior Engineer', company: 'Home Depot'},
+  {title: 'Full Stack Software Developer', company: 'AirBnB'}
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Jobs jobs ={mockJobs}/>
     </div>
   );
 }
