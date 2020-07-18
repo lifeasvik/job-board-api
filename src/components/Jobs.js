5072bc2b2eeae6e77074aa7c7b53e51e067a8fd7
Vik Birdi <vikbirdi@Viks-MacBook-Pro.local>
2020-07-18 14:31:15 -0500

    added to job

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -4,6 +4,25 @@ import Typography from '@material-ui/core/Typography';
 import Job from './Job';
 
 export default function Jobs({jobs}){
+
+    function scrollToTop () {
+        const c = document.documentElement.scrollTop || document.body.scrollTop;
+        if (c > 0) {
+          window.requestAnimationFrame(scrollToTop);
+          window.scrollTo(0, c - c / 8);
+        }
+      };
+
+    function handleNext() {
+        setActiveStep(prevActiveStep => prevActiveStep + 1);
+        scrollToTop();
+    }
+
+    function handleBack() {
+        setActiveStep(prevActiveStep => prevActiveStep - 1);
+        scrollToTop();
+    }    
+    
     return(
         <div className="jobs">
             <Typography variant="h1">
