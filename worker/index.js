da9b2cd5e21ada7274ae679f7b8f4e3c444ed2f4
Vik Birdi <vikbirdi@Viks-MacBook-Pro.local>
2020-07-18 13:10:19 -0500

    added cron, added file to pull from github jobs

---
+++ worker/index.js
@@ -0,0 +1,7 @@
+var CronJob = require('cron').CronJob;
+
+const fetchGithub = require('./tasks/fetch-github')
+
+// fetch Github Jobs
+var job = new CronJob('*/1 * * * * ', fetchGithub, null, true, 'America/Los_Angeles');
+job.start();
